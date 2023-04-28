2b31f9018413b144694c8ad357bc0a9ba2a250fa
na-talia <ntsh_89@ukr.net>
2023-04-28 20:04:33 +0200

    add index.js file

---
+++ index.js
@@ -0,0 +1,24 @@
+const ratingPage = document.querySelector(".first-page");
+const thankYouPage = document.querySelector(".second-page");
+const ratingButtons = document.querySelector(".rating-buttons");
+const selectedRating = document.getElementById("selected-rating");
+const submitButton = document.getElementById("submit-btn");
+
+ratingButtons.addEventListener("click", (e) => {
+  let selectedNumber = e.target.innerText;
+  selectedRating.innerText = selectedNumber;
+  console.log(selectedNumber);
+  submitButton.addEventListener("click", () => {
+    ratingPage.style.display = "none";
+    //thankYouPage.className = ""; - removing all classes from the element
+    thankYouPage.classList.remove("hidden"); //classList.remove() method removes an individual class/classes
+  });
+});
+
+/* doesn`t work
+  ratingButtons.forEach((rating) => {
+    rating.addEventListener("click", () => {
+      selectedRating.innerText = rating.innerText;
+    }); 
+  });
+*/
